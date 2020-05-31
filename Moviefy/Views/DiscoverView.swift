8cbf343567ab5e30a19376eaccec37deb33ae3c9
Nuno Gonçalves <nunojfg@gmail.com>
2020-05-31 11:38:28 +0100

    Added pull to refresh functionality

--- Moviefy/Views/DiscoverView.swift
+++ Moviefy/Views/DiscoverView.swift
@@ -7,11 +7,13 @@
 //
 
 import SwiftUI
+import SwiftUIPullToRefresh
 
 struct DiscoverView: View {
     @State private var searchTerm: String = ""
     @ObservedObject var networkManager = NetworkManager()
     @State private var selectedMediaType = "Movie"
+    @State private var showRefreshView = false
     var mediaTypes = ["Movie", "TV"]
     
     init() {
@@ -41,39 +43,53 @@ struct DiscoverView: View {
                 } else {
                     
                     if selectedMediaType == "Movie" {
-                        if networkManager.movies.results.isEmpty {
-                            Spacer()
-                            Text("No movies to show")
-                            Spacer()
-                        } else {
-                            List {
-                                SearchBar(text: $searchTerm)
-                                
-                                ForEach(networkManager.movies.results.filter{
-                                    self.searchTerm.isEmpty ? true : $0.title.localizedStandardContains(self.searchTerm)
-                                }, id: \.self) { movie in
-                                    NavigationLink(destination: MovieDetails(movie: movie)){
-                                        MovieRow(movie: movie)
+                        ZStack {
+                            RefreshableList(showRefreshView: $showRefreshView, action:{
+                                self.networkManager.loadData()
+                                self.showRefreshView = false
+                            }){
+                                if !self.networkManager.movies.results.isEmpty {
+                                    SearchBar(text: self.$searchTerm)
+                                    
+                                    ForEach(self.networkManager.movies.results.filter{
+                                        self.searchTerm.isEmpty ? true : $0.title.localizedStandardContains(self.searchTerm)
+                                    }, id: \.self) { tvShow in
+                                        NavigationLink(destination: MovieDetails(movie: tvShow)){
+                                            MovieRow(movie: tvShow)
+                                        }
                                     }
                                 }
                             }
+                            
+                            if self.networkManager.movies.results.isEmpty {
+                                Spacer()
+                                Text("No movies to show")
+                                Spacer()
+                            }
                         }
                     } else {
-                        if networkManager.tvShows.results.isEmpty {
-                            Spacer()
-                            Text("No tv shows to show")
-                            Spacer()
-                        } else {
-                            List {
-                                SearchBar(text: $searchTerm)
-                                
-                                ForEach(networkManager.tvShows.results.filter{
-                                    self.searchTerm.isEmpty ? true : $0.title.localizedStandardContains(self.searchTerm)
-                                }, id: \.self) { tvShow in
-                                    NavigationLink(destination: MovieDetails(movie: tvShow)){
-                                        MovieRow(movie: tvShow)
+                        ZStack {
+                            RefreshableList(showRefreshView: $showRefreshView, action:{
+                                self.networkManager.loadDataTVShows()
+                                self.showRefreshView = false
+                            }){
+                                if !self.networkManager.tvShows.results.isEmpty {
+                                    SearchBar(text: self.$searchTerm)
+                                    
+                                    ForEach(self.networkManager.tvShows.results.filter{
+                                        self.searchTerm.isEmpty ? true : $0.title.localizedStandardContains(self.searchTerm)
+                                    }, id: \.self) { tvShow in
+                                        NavigationLink(destination: MovieDetails(movie: tvShow)){
+                                            MovieRow(movie: tvShow)
+                                        }
                                     }
                                 }
+                            }
+                            
+                            if self.networkManager.tvShows.results.isEmpty {
+                                Spacer()
+                                Text("No tv shows to show")
+                                Spacer()
                             }
                         }
                     }
@@ -84,7 +100,7 @@ struct DiscoverView: View {
                 Button(action: {
                     self.networkManager.loadData()
                 }){
-                    Image(systemName: "arrow.clockwise.circle.fill")
+                    Text("Kids")
                 }
             )
         }
